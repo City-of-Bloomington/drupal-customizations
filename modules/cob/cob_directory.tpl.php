2235031bb79d4fcbf809e82d64e28b29d596ee2a
Dan Hiester <hiesterd@bloomington.in.gov>
2015-07-30 11:11:17 -0400

    Minor code changes for the public employee directory.

--- modules/cob/cob_directory.tpl.php
+++ modules/cob/cob_directory.tpl.php
@@ -5,16 +5,28 @@
  * @author Cliff Ingham <inghamn@bloomington.in.gov>
  * @param $departments The raw JSON response
  */
-$fields = ['office', 'fax', 'address', 'email'];
-foreach ($departments as $d) {
-    $name = l($d->name, 'directory/departments'.$d->path);
+ ?>
+<div class="cob-pageOverview">
+    <div class="cob-pageOverview-container">
+        <div>
+            Into text.
+        </div>
+    </div>
+</div>
+<div class="cob-main-container">
+    <?php
+        $fields = ['office', 'fax', 'address', 'email'];
+        foreach ($departments as $d) {
+            $name = l($d->name, 'directory/departments'.$d->path);
 
-    echo "<table><caption>$name</caption>";
-    foreach ($fields as $f) {
-        if (!empty($d->$f)) {
-            $value = check_plain($d->$f);
-            echo "<tr><th>$f</th><td>$value</td></tr>";
+            echo "<table><caption>$name</caption>";
+            foreach ($fields as $f) {
+                if (!empty($d->$f)) {
+                    $value = check_plain($d->$f);
+                    echo "<tr><th>$f</th><td>$value</td></tr>";
+                }
+            }
+            echo "</table>";
         }
-    }
-    echo "</table>";
-}
+    ?>
+</div>
--- modules/cob/cob_directory_listing.tpl.php
+++ modules/cob/cob_directory_listing.tpl.php
@@ -5,86 +5,91 @@
  * @author Cliff Ingham <inghamn@bloomington.in.gov>
  * @param $department JSON object for the department data
  */
+?>
 
-echo renderDepartment($department);
+<div class="cob-main-container cob-directory">
+    <?php
+        echo renderDepartment($department);
 
-function renderDepartment($d)
-{
-    $fields            = ['address', 'city', 'state', 'zip', 'email'];
-    $phoneNumberFields = ['office', 'fax', 'cell', 'other', 'pager' ];
+        function renderDepartment($d)
+        {
+            $fields            = ['address', 'city', 'state', 'zip', 'email'];
+            $phoneNumberFields = ['office', 'fax', 'cell', 'other', 'pager' ];
 
-    $name = l($d['name'], 'directory/departments'.$d['path']);
-    $html = "<section><h2>$name</h2>";
+            $name = l($d['name'], 'directory/departments'.$d['path']);
+            $html = "<section><h2>$name</h2>";
 
-    foreach ($fields as $f) {
-        $$f = check_plain($d[$f]);
-    }
-    $html.= "
-    <div><address>$address\n$city $state $zip</address></div>
-    <div><a href=\"mailto:$email\">$email</a></div>
-    <table>
-    ";
-    foreach ($phoneNumberFields as $label) {
-        $number = check_plain($d[$label]);
-        if ($number) {
-            $html.= "<tr><th>$label</th><td>$number</td></tr>";
-        }
-    }
-    $html.= "
-    </table>
-    ";
-
-    if (!empty($d['people'])) {
-        $html.= "
-        <table>
-            <thead>
-                <tr><th scope=\"col\">name</th>
-                    <th scope=\"col\">title</th>
-                    <th scope=\"col\">phone</th>
-                    <th scope=\"col\">email</th>
-                </tr>
-            </thead>
-            <tbody>
-        ";
-        foreach ($d['people'] as $p) {
-            $name =    !empty($p['displayname'])
-                ? check_plain($p['displayname'])
-                : check_plain("$p[firstname] $p[lastname]");
-
-            $title = check_plain($p['title']);
-
-            $phone = '';
-            foreach ($phoneNumberFields as $f) {
-                if (!empty($p[$f])) {
-                    $phone = $p[$f];
-                    break;
-                }
-            }
-            $email = '';
-            if (!empty($p['email'])) {
-                $email = check_plain($p['email']);
-                $email = "<a href=\"mailto:$email\">$email</a>";
+            foreach ($fields as $f) {
+                $$f = check_plain($d[$f]);
             }
             $html.= "
-            <tr><td>$name</td>
-                <td>$title</td>
-                <td class=\"ext-number\">$phone</td>
-                <td>$email</td>
-            </tr>
+            <div><address>$address\n$city $state $zip</address></div>
+            <div><a href=\"mailto:$email\">$email</a></div>
+            <table>
             ";
-        }
-        $html.= "
-            </tbody>
-        </table>
-        ";
-    }
-    $html.= "</section>";
+            foreach ($phoneNumberFields as $label) {
+                $number = check_plain($d[$label]);
+                if ($number) {
+                    $html.= "<tr><th>$label</th><td>$number</td></tr>";
+                }
+            }
+            $html.= "
+            </table>
+            ";
 
-    if (  !empty($d['departments'])) {
-        foreach ($d['departments'] as $child) {
-            $html.= renderDepartment($child);
-        }
-    }
+            if (!empty($d['people'])) {
+                $html.= "
+                <table>
+                    <thead>
+                        <tr><th scope=\"col\">name</th>
+                            <th scope=\"col\">title</th>
+                            <th scope=\"col\">phone</th>
+                            <th scope=\"col\">email</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                ";
+                foreach ($d['people'] as $p) {
+                    $name =    !empty($p['displayname'])
+                        ? check_plain($p['displayname'])
+                        : check_plain("$p[firstname] $p[lastname]");
 
-    return $html;
-}
+                    $title = check_plain($p['title']);
+
+                    $phone = '';
+                    foreach ($phoneNumberFields as $f) {
+                        if (!empty($p[$f])) {
+                            $phone = $p[$f];
+                            break;
+                        }
+                    }
+                    $email = '';
+                    if (!empty($p['email'])) {
+                        $email = check_plain($p['email']);
+                        $email = "<a href=\"mailto:$email\">$email</a>";
+                    }
+                    $html.= "
+                    <tr><td>$name</td>
+                        <td>$title</td>
+                        <td class=\"ext-number\">$phone</td>
+                        <td>$email</td>
+                    </tr>
+                    ";
+                }
+                $html.= "
+                    </tbody>
+                </table>
+                ";
+            }
+            $html.= "</section>";
+
+            if (  !empty($d['departments'])) {
+                foreach ($d['departments'] as $child) {
+                    $html.= renderDepartment($child);
+                }
+            }
+
+            return $html;
+        }
+    ?>
+</div>
