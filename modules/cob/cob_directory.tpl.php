69cbde0273b8df4e50a08b225528bfc08e689ba9
inghamn <inghamn@bloomington.in.gov>
2015-08-03 14:34:40 -0400

    Directory listing is now based off Departments that are in Drupal.

--- modules/cob/cob_directory.tpl.php
+++ modules/cob/cob_directory.tpl.php
@@ -3,7 +3,7 @@
  * @copyright 2015 City of Bloomington, Indiana
  * @license http://www.gnu.org/licenses/agpl.txt GNU/AGPL, see LICENSE.txt
  * @author Cliff Ingham <inghamn@bloomington.in.gov>
- * @param $departments The raw JSON response
+ * @param $departments Drupal Department nodes
  */
  ?>
 <div class="cob-pageOverview">
@@ -17,16 +17,19 @@
     <?php
         $fields = ['office', 'fax', 'address', 'email'];
         foreach ($departments as $d) {
-            $name = l($d->name, 'directory/departments'.$d->path);
+            if (!empty($d->field_directory_dn['und'][0]['value'])) {
+                $name = l($d->title, 'node/'.$d->nid);
+                echo "<table><caption>$name</caption>";
 
-            echo "<table><caption>$name</caption>";
-            foreach ($fields as $f) {
-                if (!empty($d->$f)) {
-                    $value = check_plain($d->$f);
-                    echo "<tr><th>$f</th><td>$value</td></tr>";
+                $info = cob_department_info($d->field_directory_dn['und'][0]['value']);
+                foreach ($fields as $f) {
+                    if (!empty($info->$f)) {
+                        $value = check_plain($info->$f);
+                        echo "<tr><th>$f</th><td>$value</td></tr>";
+                    }
                 }
+                echo "</table>";
             }
-            echo "</table>";
         }
     ?>
 </div>
