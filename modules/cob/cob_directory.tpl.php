353ed45912b682974781fe3232090a897bd91612
hiesterd <hiesterd@bloomington.in.gov>
2015-08-05 07:01:25 -0700

    Preliminary work on the Staff Directory page.

--- modules/cob/cob_directory.tpl.php
+++ modules/cob/cob_directory.tpl.php
@@ -13,22 +13,31 @@
         </div>
     </div>
 </div>
-<div class="cob-main-container">
+<div class="cob-main-container cob-directory">
     <?php
         $fields = ['office', 'fax', 'address', 'email'];
         foreach ($departments as $d) {
             if (!empty($d->field_directory_dn['und'][0]['value'])) {
-                $name = l($d->title, 'node/'.$d->nid);
-                echo "<table><caption>$name</caption>";
+                echo '<section class="cob-directory-section">';
+                    $name = l($d->title, 'node/'.$d->nid);
+                    echo '<h2>'.$name.'</h2>';
+                    echo '<div>';
 
-                $info = cob_department_info($d->field_directory_dn['und'][0]['value']);
-                foreach ($fields as $f) {
-                    if (!empty($info->$f)) {
-                        $value = check_plain($info->$f);
-                        echo "<tr><th>$f</th><td>$value</td></tr>";
-                    }
-                }
-                echo "</table>";
+                        echo '<div class="cob-directory-deptInfo">';
+                            echo '<dl>';
+
+                                $info = cob_department_info($d->field_directory_dn['und'][0]['value']);
+                                foreach ($fields as $f) {
+                                    if (!empty($info->$f)) {
+                                        $key   = ucfirst($f);
+                                        $value = check_plain($info->$f);
+                                        echo "<dt>$key</dt><dd>$value</dd>";
+                                    }
+                                }
+                            echo '</dl>';
+                        echo '</div>';
+                    echo '</div>';
+                echo '</section>';
             }
         }
     ?>
