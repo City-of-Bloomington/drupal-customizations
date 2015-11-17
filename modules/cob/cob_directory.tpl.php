53eb71c37bf5187906584b034a3ad726be535013
inghamn <inghamn@bloomington.in.gov>
2015-11-17 14:53:03 -0500

    Added real data to the example directory markup

--- modules/cob/cob_directory.tpl.php
+++ modules/cob/cob_directory.tpl.php
@@ -14,59 +14,56 @@
     </div>
 </div>
 <div class="cob-main-container cob-directory">
-    <?php
-        $fields = ['office'/*, 'fax'*/, 'address', 'email'];
-        foreach ($departments as $d) {
-            if (!empty($d->field_directory_dn['und'][0]['value'])) {
-                echo '<section class="cob-directory-section">';
-                    $name = l($d->title, 'node/'.$d->nid);
-                    echo '<h2>'.$name.'</h2>';
-
-                    echo '<div class="cob-directory-deptInfo">';
-                        echo '<dl>';
-
-                            $info = cob_department_info($d->field_directory_dn['und'][0]['value']);
-                            foreach ($fields as $f) {
-                                if (!empty($info->$f)) {
-                                    $key   = ucfirst($f);
-                                    $value = check_plain($info->$f);
-                                    echo "<dt>$key</dt><dd>$value</dd>";
-                                }
-                            }
-                        echo '</dl>';
-                    echo '</div>';
-                ?>
-                    <div class="cob-directory-deptStaff">
-                        <table>
-                            <thead>
-                                <tr>
-                                    <td>Name and title</td>
-                                    <td>Email address</td>
-                                    <td>Phone Number</td>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <tr>
-                                    <td>First Last <span class="cob-directory-title">Director</span></td>
-                                    <td><a href="mailto:lastf@bloomington.in.gov">lastf@bloomington.in.gov</a></td>
-                                    <td>812-349-3454</td>
-                                </tr>
-                                <tr>
-                                    <td>First Last <span class="cob-directory-title">Director</span></td>
-                                    <td><a href="mailto:lastf@bloomington.in.gov">lastf@bloomington.in.gov</a></td>
-                                    <td>812-349-3454</td>
-                                </tr>
-                                <tr>
-                                    <td>First Last <span class="cob-directory-title">Director</span></td>
-                                    <td><a href="mailto:lastf@bloomington.in.gov">lastf@bloomington.in.gov</a></td>
-                                    <td>812-349-3454</td>
-                                </tr>
-                            </tbody>
-                        </table>
-                    </div>
-                <?php
-                echo '</section>';
-            }
+<?php
+    $deptContactFields = ['office', 'address', 'email'];
+    foreach ($departments as $d) {
+        if (!empty($d->field_directory_dn['und'][0]['value'])) {
+            $name = l($d->title, 'node/'.$d->nid);
+            echo "
+            <section class=\"cob-directory-section\">
+                <h1>$name</h1>
+                <div class=\"cob-directory-deptInfo\">
+                    <dl>
+            ";
+                    $info = cob_department_info($d->field_directory_dn['und'][0]['value']);
+                    foreach ($deptContactFields as $f) {
+                        if (!empty($info->$f)) {
+                            $key   = ucfirst($f);
+                            $value = check_plain($info->$f);
+                            echo "<dt>$key</dt><dd>$value</dd>";
+                        }
+                    }
+            echo "
+                    </dl>
+                </div>
+                <div class=\"cob-directory-deptStaff\">
+                    <table>
+                        <thead>
+                            <tr><td>Name and title</td>
+                                <td>Email address</td>
+                                <td>Phone Number</td>
+                            </tr>
+                        </thead>
+                        <tbody>
+            ";
+                    if (isset($info->people)) {
+                        foreach ($info->people as $p) {
+                            $name = $p->displayname ? $p->displayname : "{$p->firstname} {$p->lastname}";
+                            echo "
+                            <tr><td>$name <span class=\"cob-directory-title\">{$p->title}</span></td>
+                                <td><a href=\"mailto:{$p->email}\">{$p->email}</a></td>
+                                <td>{$p->office}</td>
+                            </tr>
+                            ";
+                        }
+                    }
+            echo "
+                        </tbody>
+                    </table>
+                </div>
+            </section>
+            ";
         }
-    ?>
+    }
+?>
 </div>
