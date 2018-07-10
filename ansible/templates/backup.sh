b56418b1f984b028105f05a01ed58c556d62dcee
inghamn <inghamn@bloomington.in.gov>
2018-07-10 09:58:07 -0400

    Ansible now deploys the backups script
    Fixes #26

---
+++ ansible/templates/backup.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+# Creates a tarball containing a full snapshot of the data in the site
+#
+# @copyright 2011-2018 City of Bloomington, Indiana
+# @license http://www.gnu.org/copyleft/gpl.html GNU/GPL, see LICENSE.txt
+APPLICATION_NAME=drupal
+MYSQL_CREDENTIALS=/etc/cron.daily/backup.d/$APPLICATION_NAME.cnf
+BACKUP_DIR={{ drupal_backup_path }}
+APPLICATION_HOME={{ drupal_install_path }}
+SITE_HOME={{ drupal_site_home }}
+LOG_FILE=/var/log/cron/$APPLICATION_NAME
+
+MYSQL_DBNAME=$APPLICATION_NAME
+
+# How many days worth of tarballs to keep around
+num_days_to_keep=5
+
+#----------------------------------------------------------
+# No Editing Required below this line
+#----------------------------------------------------------
+now=`date +%s`
+today=`date +%F`
+
+cd $BACKUP_DIR
+mkdir $today
+
+# Dump the database
+mysqldump --defaults-extra-file=$MYSQL_CREDENTIALS $MYSQL_DBNAME > $SITE_HOME/$MYSQL_DBNAME.sql
+
+# Tarball the Data
+# Dereference the symlink for the default site to the data dir
+cd $APPLICATION_HOME
+cd ..
+tar -czhf $today.tar.gz $APPLICATION_NAME
+mv $today.tar.gz $BACKUP_DIR
+
+# Purge any backup tarballs that are too old
+for file in `ls`
+do
+	atime=`stat -c %Y $file`
+	if [ $(( $now - $atime >= $num_days_to_keep*24*60*60 )) = 1 ]
+	then
+		rm $file
+	fi
+done
+
+touch $LOG_FILE
